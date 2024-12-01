95c95c5f1b16a327ecdf8bcfae78af6beaa61dd7
Tarun chintakunta <tarunchintakunta@gmail.com>
2024-12-01 11:51:06 +0530

    fixed duplicates constructors

--- src/main/java/com/monocept/myapp/dto/EmployeeResponseDto.java
+++ src/main/java/com/monocept/myapp/dto/EmployeeResponseDto.java
@@ -1,59 +1,76 @@
 package com.monocept.myapp.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class EmployeeResponseDto {
-	private long employeeId;
+    private long employeeId;
     private String firstName;
     private String lastName;
     private boolean active;
     private String email;
-	public long getEmployeeId() {
-		return employeeId;
-	}
-	public void setEmployeeId(long employeeId) {
-		this.employeeId = employeeId;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public boolean isActive() {
-		return active;
-	}
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public EmployeeResponseDto(long employeeId, String firstName, String lastName, boolean active, String email) {
-		super();
-		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.active = active;
-		this.email = email;
-	}
-	public EmployeeResponseDto() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-    
+
+    // Default Constructor
+    public EmployeeResponseDto() {
+        super();
+    }
+
+    // Parameterized Constructor
+    public EmployeeResponseDto(long employeeId, String firstName, String lastName, boolean active, String email) {
+        super();
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.active = active;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeResponseDto{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", active=" + active +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
